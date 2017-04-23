6f4ce4186e12dc69e59add46dec1d0f46a27a35e
Claudio Bandera <cbandera@posteo.de>
2017-04-23 12:42:23 +0200

    Added autocompletion for ba

--- bash_aliases.d/aliases.sh
+++ bash_aliases.d/aliases.sh
@@ -20,5 +20,40 @@ alias uc='mrt maintenance credentials update_cache'
 
 # Editing Bashrc
 bs() { echo "Sourcing ~/.bash_rc" && . ~/.bashrc; }
-alias be='nano ~/.bashrc'
-alias ba='nano ~/.bash_aliases.d/aliases.sh'
+alias be='$EDITOR ~/.bashrc'
+ba() {
+	if [ ! $@ > 1 ]; then
+		$EDITOR ~/.bash_aliases.d/aliases.sh
+	else
+		BM_ROOT_DIR=$( cd "$(readlink -f $( dirname "${BASH_SOURCE[0]}" ))/.." && pwd );
+    	$EDITOR ${BM_ROOT_DIR}/bash_${1}.d/${2}.sh
+	fi
+}
+
+_ba_complete()
+{
+    local cur prev opts
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    BM_ROOT_DIR=$( cd "$(readlink -f $( dirname "${BASH_SOURCE[0]}" ))/.." && pwd );
+    if [ "$COMP_CWORD" == "1" ]; then
+		BM_OPT_DIRS=$(find $BM_ROOT_DIR -maxdepth 1 -type d -name "bash_*.d" | grep -o "[^/]*\.d" | grep -o "_[a-z]*\." |grep -o "[a-z]*" )
+		opts=$BM_OPT_DIRS
+	elif [ "$COMP_CWORD" == "2" ]; then
+		BM_OPT_FILES=$(find ${BM_ROOT_DIR}/bash_${prev}.d/ -type f -name "*.sh" | grep -o "[^/]*\.sh" | grep -o "^[^\.]*")
+		opts=$BM_OPT_FILES
+	fi
+#	echo
+#	echo "COMP_WORDS: $COMP_WORDS"
+#	echo "COMP_CWORD: $COMP_CWORD"
+#	echo "cur: $cur"
+#	echo "prev: $prev"
+#	echo "opts: $opts"
+	
+
+	COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+    return 0
+}
+
+complete -F _ba_complete ba
