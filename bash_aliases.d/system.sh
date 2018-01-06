169292441cfd862b75ab56c9b7398659f9314f95
Claudio Bandera <cbandera@posteo.de>
2018-01-06 10:49:44 +0100

    Added apt command

--- bash_aliases.d/system.sh
+++ bash_aliases.d/system.sh
@@ -6,3 +6,8 @@ updatesys() {
 	sudo apt-get dist-upgrade -y;	alert;
 	sudo apt-get autoremove; 	alert;
 }
+
+# List all manually installed packages
+alias listManuallyInstalledPackages="comm -23 <(apt-mark showmanual | sort -u) <(gzip -dc /var/log/installer/initial-status.gz | sed -n 's/^Package: //p' | sort -u)"
+
+
